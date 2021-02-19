846f6c9b2d60ac2e51ea4fc42b8ff401bda90435
zgjsxx <xu_zhang@trendmicro.com>
2021-02-19 10:06:56 +0800

    add springmvc jsp template

---
+++ springmvc-jsp-template/src/main/java/com/it/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.it.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/test")
+public class HelloController{
+    @RequestMapping(method = RequestMethod.GET)
+    public String  printHello(ModelMap model) {
+        System.out.println("add message");
+        model.addAttribute("message", "Hello Spring MVC Framework!");
+        return "hello";
+
+    }
+}
+
+
---
+++ springmvc-jsp-template/src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" isELIgnored="false" %>
+<html>
+<head>
+    <title>Index</title>
+</head>
+<body>
+<h2>${message}</h2>
+</body>
+</html>
